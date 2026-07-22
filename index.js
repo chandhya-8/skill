f83efec8b8941a90eeb5198c59fcc7cbe949bb59
chandhya-8 <chandhyapentagon8@gmail.com>
2026-07-22 13:06:56 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
-/**
- * Detect Electron renderer / nwjs process, which is node, but we should
- * treat as a browser.
- */
+var versions = require('./versions');
+var fullVersions = require('./full-versions');
+var chromiumVersions = require('./chromium-versions');
+var fullChromiumVersions = require('./full-chromium-versions');
 
-if (typeof process === 'undefined' || process.type === 'renderer' || process.browser === true || process.__nwjs) {
-	module.exports = require('./browser.js');
-} else {
-	module.exports = require('./node.js');
-}
+var electronToChromium = function (query) {
+  var number = getQueryString(query);
+  return number.split('.').length > 2 ? fullVersions[number] : versions[number] || undefined;
+};
+
+var chromiumToElectron = function (query) {
+  var number = getQueryString(query);
+  return number.split('.').length > 2 ? fullChromiumVersions[number] : chromiumVersions[number] || undefined;
+};
+
+var electronToBrowserList = function (query) {
+  var number = getQueryString(query);
+  return versions[number] ? "Chrome >= " + versions[number] : undefined;
+};
+
+var getQueryString = function (query) {
+  var number = query;
+  if (query === 1) { number = "1.0" }
+  if (typeof query === 'number') { number += ''; }
+  return number;
+};
+
+module.exports = {
+  versions: versions,
+  fullVersions: fullVersions,
+  chromiumVersions: chromiumVersions,
+  fullChromiumVersions: fullChromiumVersions,
+  electronToChromium: electronToChromium,
+  electronToBrowserList: electronToBrowserList,
+  chromiumToElectron: chromiumToElectron
+};
